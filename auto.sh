99e0ed0b6e5b71a25703458f01a325655f1e5e54
Gentleman.Hu <justfeelingme@gmail.com>
2020-09-11 10:35:37 +0000

    short bot msg,color git msg

--- auto.sh
+++ auto.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 git add -A;
 
 if [[ $1 ]];then
     git commit -am "$1"
+    echo -e "update message:${RED} $1 ${NC}"
 else
     git commit -am "update"
 fi
