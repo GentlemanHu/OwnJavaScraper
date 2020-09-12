f00db5a4e3afdf9c8cd8937dd2b8609d23b1347d
Gentleman.Hu <justfeelingme@gmail.com>
2020-09-12 11:23:44 +0000

    first github demo in test

---
+++ way/src/test/java/god/GithubRepoPageProcessor.java
@@ -0,0 +1,30 @@
+package god;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class GithubRepoPageProcessor implements PageProcessor {
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    public void process(Page page) {
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if(page.getResultItems().get("name")==null){
+            page.setSkip(true);
+        }
+
+        page.putField("readme",page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
+    }
+
+    @Override
+    public Site getSite() {
+        // TODO Auto-generated method stub
+        return site;
+    }
+    
+    
+}
